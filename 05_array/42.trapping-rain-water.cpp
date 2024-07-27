43d164473892e929bac1e28dc22010ce2ea37391
timmy <zhuzhonghua@yingzi.com>
2024-07-27 10:13:54 +0800

    接雨水

--- 05_array/42.trapping-rain-water.cpp
+++ 05_array/42.trapping-rain-water.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -143,8 +144,12 @@ int trap2(vector<int> &height)
  * -- 动态规划的状态转移方程式： leftMax[i]表示柱子i左侧的最高柱子的高度 = max(leftMax[i-1],heiht[i])
  * - 右侧的最高柱子高度也是一样的
  * - 最后根据这两个数组，计算每个柱子能够接到的水量， = min(leftMax[i],rightMax[i]) - height[i]
+ *
+ * 3、其他解法
+ * - 单调栈解法
+ * - 双指针解法
  */
-int trap(vector<int> &height)
+int trap3(vector<int> &height)
 {
     int count = height.size();
     vector<int> leftMax(count, 0);
@@ -171,9 +176,76 @@ int trap(vector<int> &height)
     return sum;
 }
 
+/**
+ * 单调栈解法：
+ * - 使用一个栈来保存柱子的索引，栈中保存的柱子高度需要是递减的，
+ * - 不断遍历数组，遇到比栈顶元素小的值，索引入栈
+ * - 当遇到比栈顶元素大于等于的值，则需要计算能接到雨水的数量，出现这种情况说明栈中元素一定存在至少两个元素，
+ * -- 要形成单调栈，则栈中一定要至少两个元素，如果只有一个元素，当遇到比栈顶元素大的值，说明是连续上升的情况，直接全部出栈，将最新的最大的值入栈
+ * -- 如果栈中有两个元素，且出现的值比栈顶元素大，则计算能接到的雨水量
+ */
+
+/**
+// 1、取出栈顶元素进行比较，
+// - 比栈顶元素小的，入栈
+// - 比栈顶元素大的，继续判断元素个数
+// 2、判断元素个数
+// - 栈中只有一个元素，来了一个比栈顶元素大的值，要保持单调递减栈，需要出栈后，将新值入栈
+// - 如果栈中元素大于等于两个，计算能接到的雨水量，雨水量等于： 宽度*落差值
+--  落差值为： min（栈中top-1，新的值i）-栈顶元素值top ， --》最后栈顶元素出栈
+*  - 继续与栈顶元素比较，让这新值不断与新出现的栈顶元素比较最后的结果，要么入栈维持单调递减，要么全部出栈，新值入栈作为栈顶元素
+*/
+int trap(vector<int> &height)
+{
+    int count = height.size();
+    stack<int> stack;
+    stack.push(0);
+    int sum = 0;
+
+    for (int i = 1; i < count; i++)
+    {
+        int topI = stack.top();
+        // 如果比栈顶元素小，直接入栈
+        if (height[i] < height[stack.top()])
+        {
+            stack.push(i);
+            continue;
+        }
+
+        // 比栈顶元素大，不断出栈了，知道栈顶元素比当前值大，然后入栈
+        while (height[stack.top()] < height[i])
+        {
+            // 如果只有一个的话，直接出栈，新值入栈
+            if (stack.size() == 1)
+            {
+                stack.pop();
+                stack.push(i);
+                break;
+            }
+
+            // 2个及以上，计算雨水量
+            // 出栈
+            int popI = stack.top();
+            stack.pop();
+
+            // 前一个更大高度的柱子下标
+            int leftI = stack.top();
+
+            // 计算雨水量
+            int rains = (i - leftI - 1) * (min(height[i], height[leftI]) - height[popI]);
+            sum += rains;
+
+            cout << "while i:" << i << ",popI:" << popI << ",leftI:" << leftI << ",rains:" << rains << endl;
+        }
+        stack.push(i);
+    }
+    return sum;
+}
+
 int main(int argc, char const *argv[])
 {
     std::vector<int> height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    // std::vector<int> height = {4, 2, 0, 3, 2, 5};
     int res = trap(height);
     cout << "res:" << res << endl;
 
