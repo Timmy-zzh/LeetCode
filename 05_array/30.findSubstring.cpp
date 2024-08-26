1f68cd31457b4c8816095a71a2387e6cab338cd3
timmy <zhuzhonghua@yingzi.com>
2024-08-26 09:19:54 +0800

    范围区间，字符串组成

--- 05_array/30.findSubstring.cpp
+++ 05_array/30.findSubstring.cpp
@@ -7,6 +7,100 @@
 using namespace std;
 
 /**
+ * 1、审题：
+ * - 输入一个字符串s和一个字符串数组words，从字符串s中找到所有目标子串，子串要求由字符串数组words中的所有单词组成，不管单词顺序
+ * 2、解题：
+ * - 暴力解法，遍历字符串s，每次截取words数组长度个单词的子串，判断截取的子串是否与words数组中的单词匹配
+ * - 遍历开始位置，需要从0到words数组中每个单词长度的位置，
+ * - 遍历的子串，和words中数组中的字符串，是否匹配判断，可通过map集合来保存，以words数组中的单词和对应个数为标准，
+ * -- 然后遍历截取的子串中每个单词，每遍历一个单词，map对应单词的个数减1，知道最后看map集合中的key值是否为空。为空表示匹配条件
+ * 3、超过时间限制
+ * - 需要做优化
+ */
+vector<int> findSubstring(string s, vector<string> &words)
+{
+    int m = words.size();
+    int n = words[0].size();
+    int sLen = s.length();
+    vector<int> res;
+
+    map<string, int> mapArr;
+    for (int i = 0; i < m; i++)
+    {
+        string word = words[i];
+        if (mapArr.find(word) != mapArr.end()) // 包含，值在原有基础上加1
+        {
+            mapArr[word] = mapArr[word] + 1;
+        }
+        else
+        {
+            mapArr[word] = 1;
+        }
+    }
+
+    // 打印map集合
+    for (auto it = mapArr.begin(); it != mapArr.end(); it++)
+    {
+        cout << "key:" << it->first << ",value:" << it->second << endl;
+    }
+
+    // 先从字符串第0个字符开始截取，每次截取 words所有单词的长度，
+    // int start = 0; // 开始位置，不一定是从0开始的，应该是动态的从[0,n]
+    for (int j = 0; j < n; j++)
+    {
+        int start = j;
+        while (start + (m * n) <= sLen) // start表示开始位置，以i为起点，遍历m*n个字符作为滑动窗口去截取字符串
+        {
+            cout << "----start:" << start << endl;
+            // 深拷贝一个map集合，用于操作
+            map<string, int> mapTemp(mapArr);
+            for (int j = 0; j < m; j++)
+            {
+                // 截取字符串
+                string subStr = s.substr(start + j * n, n);
+                cout << "subStr:" << subStr << endl;
+                if (mapTemp.find(subStr) == mapTemp.end()) // 判断截取的子串，在mapTemp集合中是否存在，不存在，则start更新遍历位置，从新遍历
+                {
+                    start = start + j * n;
+                    cout << "not exist start:" << start << endl;
+                    break;
+                }
+
+                // 存在，处理mapTemp集合的单词对应个数，如果个数为0，则删除这个key
+                // mapTemp[subStr] = mapTemp[subStr] - 1;
+                if (mapTemp.find(subStr) != mapTemp.end()) // 存在
+                {
+                    mapTemp[subStr] = mapTemp[subStr] - 1;
+                }
+                else
+                {
+                    mapTemp[subStr] = 1;
+                }
+            }
+
+            // 判断mapTemp集合中key值对应的个数是否都为0，都为0，则找到满足条件的子串
+            bool isMatch = true;
+            for (auto it = mapTemp.begin(); it != mapTemp.end(); it++)
+            {
+                if (it->second != 0)
+                {
+                    isMatch = false;
+                    break;
+                }
+            }
+            if (isMatch)
+            {
+                res.push_back(start);
+            }
+
+            start = start + n;
+        }
+    }
+
+    return res;
+}
+
+/**
  * 解题：
  * - 使用map保存滑动窗口中的单词，是否已经全部使用过了
  * - 只要遍历滑动窗口范围内的所有单词，也就是map中标记的单词是否已经全部使用过了，
@@ -19,18 +113,19 @@ using namespace std;
  * -- 遍历过程中，暴力解法，每次截取单词，都需要与基础集合mapArr进行比较，查看是否满足条件
  * -- 如果遇到截取的单词在mapArr中不存在，则直接跳过，从新开始遍历，滑动窗口移动到新的位置，maps集合清空后再填充
  */
-vector<int> findSubstring(string s, vector<string> &words)
+/**
+ vector<int> findSubstring2(string s, vector<string> &words)
 {
     cout << "s:" << s << endl;
     vector<int> res;
     int arrSize = words.size();           // 数组长度
     int itemLen = words[0].size();        // 数组中一个单词的长度
     int strLen = s.length();              // 字符串长度
-    map<bool> mapExist;                   // 标记数组，默认所有单词都没有遍历过,key为单词在数组s中的索引位置，vulue标记这个单词是否已经使用过了
+    vector<bool> mapExist;                   // 标记数组，默认所有单词都没有遍历过,key为单词在数组s中的索引位置，vulue标记这个单词是否已经使用过了
     vector<bool> visited(arrSize, false); // 标记数组，默认所有单词都没有遍历过
 
     // 过滤字符串s长度过小的情况
-    if (count < arrSize * itemLen)
+    if (strLen < arrSize * itemLen)
     {
         return res;
     }
@@ -58,10 +153,8 @@ vector<int> findSubstring(string s, vector<string> &words)
         i = i + itemLen;
     }
 
-    /**
-     * 遍历map集合，一次遍历的个数是words数组的长度，组成一个滑动窗口，后面再移动的时候，
-     * 由窗口负责增加一个单词，和减少一个单词的使用标记
-     */
+     // 遍历map集合，一次遍历的个数是words数组的长度，组成一个滑动窗口，后面再移动的时候，
+     // 由窗口负责增加一个单词，和减少一个单词的使用标记
     // fill(mapUsed.begin(), mapUsed.end(), false);
     fill(visited.begin(), visited.end(), false);
 
@@ -127,6 +220,7 @@ vector<int> findSubstring(string s, vector<string> &words)
 
     return res;
 }
+ */
 
 /**
  * 1、审题： 给定一个字符串s和一个字符串数组words，字符串s的子串，由words中的所有字符组成，
@@ -231,8 +325,11 @@ int main(int argc, char const *argv[])
     // vector<string> words = {"foo", "bar"};
     // vector<int> res = findSubstring("barfoothefoobarman", words);
 
-    vector<string> words = {"bar", "foo", "the"};
-    vector<int> res = findSubstring("barfoofoobarthefoobarman", words);
+    // vector<string> words = {"bar", "foo", "the"};
+    // vector<int> res = findSubstring("barfoofoobarthefoobarman", words);
+
+    vector<string> words = {"fooo", "barr", "wing", "ding", "wing"};
+    vector<int> res = findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", words);
     for (auto r : res)
     {
         cout << "r:" << r << endl;
---
+++ 05_array/54.spiral-matrix.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+using namespace std;
+
+/**
+ * 1、审题：输入一个二维m*n矩阵，以螺旋路线查询所有元素，并返回一个数组
+ * 2、解题：
+ * - 螺旋式遍历矩阵元素，从外层到内层，遍历的时候如何转角呢？
+ * - 
+ */
+vector<int> spiralOrder(vector<vector<int>> &matrix)
+{
+}
+
+/**
+ * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+
+示例 1：
+输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+输出：[1,2,3,6,9,8,7,4,5]
+
+示例 2：
+输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ */
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
